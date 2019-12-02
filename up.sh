9a95f00a343bcf370eef4344342adc6e543bc9bf
Tristan Horlamus <tristan.horlamus@googlemail.com>
2019-12-02 16:00:16 -0500

    created Dockerfiles for each image to add potential configs locally

--- up.sh
+++ up.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker-compose up -d
+#openssl genrsa -out portainer.key 2048 
+#openssl ecparam -genkey -name secp384r1 -out portainer.key 
+#openssl req -new -x509 -sha256 -key portainer.key -out portainer.crt -days 3650 
+
+docker-compose up -d --build
